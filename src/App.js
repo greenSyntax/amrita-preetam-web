746b8c6d3fff8671dfc8be38180a21cfed2f2ec0
Abhishek Ravi <ab.abhishek.ravi@gmail.com>
2023-10-01 22:22:10 +0530

    Added Detail Screen, About and Contact

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import AboutPage from './components/AboutPage';
+import Amrita from './components/Amrita';
+import About from './components/About';
 import PostList from './components/PostList';
 import PostDetail from './components/PostDetail';
+import Detail from './components/Detail';
 import Contact from './components/Contact';
+import Contribution from './components/Contribution';
+import Report from './components/Report';
 
 function App() {
   return (
@@ -13,10 +17,13 @@ function App() {
         <Routes>
           <Route exact path="/" Component={PostList}></Route>
           <Route path="/list" Component={PostList}></Route>
-          <Route path="/about" extact Component={AboutPage}></Route>
+          <Route path="/about" extact Component={About}></Route>
           <Route path="/post/" Component={PostList}></Route>
-          <Route path="/post/:postId" Component={PostDetail}></Route>
+          <Route path="/post/:postId" Component={Detail}></Route>
           <Route path="/contact" Component={Contact}></Route>
+          <Route path="/contribution" Component={Contribution}></Route>
+          <Route path="/amrita" Component={Amrita}></Route>
+          <Route path="/report" Component={Report}></Route>
         </Routes>
       </div>
     </BrowserRouter>
